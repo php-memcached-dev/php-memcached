dbb68a5c597b8eba367053f172e3c7f7a20f8942
Mikko <mikko@kuut.io>
2013-11-18 16:12:12 +0800

    More travis stuff

--- travis.sh
+++ travis.sh
@@ -14,7 +14,9 @@ function validate_package_xml() {
 
 function install_libmemcached() {
     local libmemcached_version=$1
-    local enable_protocol=$2
+    local libmemcached_prefix=$2
+    local enable_protocol=$3
+    
     wget "https://launchpad.net/libmemcached/1.0/${libmemcached_version}/+download/libmemcached-${libmemcached_version}.tar.gz" -O libmemcached-${libmemcached_version}.tar.gz
 
     tar xvfz libmemcached-${libmemcached_version}.tar.gz
@@ -41,28 +43,34 @@ function install_igbinary() {
 }
 
 function build_php_memcached() {
+    local libmemcached_prefix=$1
+    local php_memcached_version=$2
+    local enable_protocol=$3
+
     pear package
     mkdir /tmp/php-memcached-build
-    tar xfz "memcached-${PHP_MEMCACHED_VERSION}.tgz" -C /tmp/php-memcached-build
-    pushd "/tmp/php-memcached-build/memcached-${PHP_MEMCACHED_VERSION}"
+    tar xfz "memcached-${php_memcached_version}.tgz" -C /tmp/php-memcached-build
+    pushd "/tmp/php-memcached-build/memcached-${php_memcached_version}"
         phpize
 
         if test "x$enable_protocol" = "xyes"; then
             protocol_flag="--enable-memcached-protocol"
         fi
 
-        ./configure --with-libmemcached-dir="$PHP_LIBMEMCACHED_PREFIX" $protocol_flag --enable-memcached-json --enable-memcached-igbinary
+        ./configure --with-libmemcached-dir="$libmemcached_prefix" $protocol_flag --enable-memcached-json --enable-memcached-igbinary
         make
         make install
     popd
 }
 
 function run_memcached_tests() {
+    local php_memcached_version=$1
+
     export NO_INTERACTION=1
     export REPORT_EXIT_STATUS=1
     export TEST_PHP_EXECUTABLE=`which php`
 
-    pushd /tmp/php-memcached-build
+    pushd "/tmp/php-memcached-build/memcached-${php_memcached_version}"
         php run-tests.php -d extension=igbinary.so -d extension=memcached.so -n ./tests/*.phpt
         for i in `ls tests/*.out 2>/dev/null`; do echo "-- START ${i}"; cat $i; echo ""; echo "-- END"; done
     popd
@@ -71,11 +79,11 @@ function run_memcached_tests() {
 # Command line arguments
 PHP_LIBMEMCACHED_VERSION=$1
 
-# Export the extension version
-export PHP_MEMCACHED_VERSION=$(php -r '$sxe = simplexml_load_file ("package.xml"); echo (string) $sxe->version->release;')
+# the extension version
+PHP_MEMCACHED_VERSION=$(php -r '$sxe = simplexml_load_file ("package.xml"); echo (string) $sxe->version->release;')
 
 # Libmemcached install dir
-export PHP_LIBMEMCACHED_PREFIX="${HOME}/libmemcached-${PHP_LIBMEMCACHED_VERSION}"
+PHP_LIBMEMCACHED_PREFIX="${HOME}/libmemcached-${PHP_LIBMEMCACHED_VERSION}"
 
 # Check whether to enable building with protoocol support
 dpkg --compare-versions "$PHP_LIBMEMCACHED_VERSION" '>' 1.0.15
@@ -91,14 +99,14 @@ echo "Enable protocol: $ENABLE_PROTOOCOL"
 validate_package_xml
 
 # Install libmemcached version
-install_libmemcached $PHP_LIBMEMCACHED_VERSION $ENABLE_PROTOOCOL
+install_libmemcached $PHP_LIBMEMCACHED_VERSION $PHP_LIBMEMCACHED_PREFIX $ENABLE_PROTOOCOL
 
 # Install igbinary extension
 install_igbinary
 
 # Build the extension
-build_php_memcached $ENABLE_PROTOOCOL
+build_php_memcached $PHP_LIBMEMCACHED_PREFIX $PHP_MEMCACHED_VERSION $ENABLE_PROTOOCOL
 
 # Run tests
-run_memcached_tests $PHP_LIBMEMCACHED_VERSION
+run_memcached_tests $PHP_MEMCACHED_VERSION
 
