b4103acd0356db74518534c5da1702c72b38d459
Aaron Stone <aaron@serendipity.cx>
2018-12-21 04:51:44 -0800

    Bump version to 3.1.1

--- php_memcached.h
+++ php_memcached.h
@@ -30,7 +30,7 @@
 # include "config.h"
 #endif
 
-#define PHP_MEMCACHED_VERSION "3.1.0"
+#define PHP_MEMCACHED_VERSION "3.1.1"
 
 #if defined(PHP_WIN32) && defined(MEMCACHED_EXPORTS)
 #define PHP_MEMCACHED_API __declspec(dllexport)
