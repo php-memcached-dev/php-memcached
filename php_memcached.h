5bb007e62503cf2730514db742a3d47b2d28f9be
Remi Collet <remi@remirepo.net>
2022-03-07 14:47:06 +0100

    Prepare 3.2.0RC1

--- php_memcached.h
+++ php_memcached.h
@@ -30,7 +30,7 @@
 # include "config.h"
 #endif
 
-#define PHP_MEMCACHED_VERSION "3.1.5"
+#define PHP_MEMCACHED_VERSION "3.2.0RC1"
 
 #if defined(PHP_WIN32) && defined(MEMCACHED_EXPORTS)
 #define PHP_MEMCACHED_API __declspec(dllexport)
