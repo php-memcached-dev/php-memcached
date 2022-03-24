09e094d3909ceb97bd98ccb658b5a7b6637650a4
Remi Collet <remi@remirepo.net>
2022-03-24 14:02:06 +0100

    back to dev

--- php_memcached.h
+++ php_memcached.h
@@ -30,7 +30,7 @@
 # include "config.h"
 #endif
 
-#define PHP_MEMCACHED_VERSION "3.2.0"
+#define PHP_MEMCACHED_VERSION "3.2.1-dev"
 
 #if defined(PHP_WIN32) && defined(MEMCACHED_EXPORTS)
 #define PHP_MEMCACHED_API __declspec(dllexport)
